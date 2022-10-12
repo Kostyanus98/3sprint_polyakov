281feccbd28c367e8affa51646f673273bbe5477
Kostyanus98 <kostya.pkd@ya.ru>
2022-10-13 00:59:53 +0300

    First commit
    First commit

---
+++ SchetchikPKD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SchetchikPKD
+//
+//  Created by Константин on 07.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
